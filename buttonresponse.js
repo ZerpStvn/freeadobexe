be68d3c51788d164f0c815c30708a4a3ed04dbd9
ZerpStvn <stevefelizardo4@gmail.com>
2021-02-07 19:25:13 +0800

    website

---
+++ buttonresponse.js
@@ -0,0 +1,31 @@
+var btnPsd= document.getElementById("photoshop")
+var btnLR= document.getElementById("abodeLR")
+var btnIL= document.getElementById("abodeIL")
+var btnPP= document.getElementById("abodePP")
+var btnAE= document.getElementById("abodeAE")
+var btnXD= document.getElementById("abodeXD")
+var btnsite= document.getElementById("sitepage")
+btnPsd.onclick = function(){
+    window.location ="https://clippingpanda.com/photoshop-cc-2019-crack/";
+}
+
+btnLR.onclick = function(){
+    window.location ="http://movincle.com/Vos";
+}
+btnIL.onclick = function(){
+    window.location ="http://movincle.com/WMu";
+}
+btnPP.onclick = function(){
+    window.location ="http://movincle.com/RS2";
+}
+btnAE.onclick = function(){
+    window.location ="http://movincle.com/XQG";
+}
+btnXD.onclick = function(){
+    window.location ="http://movincle.com/XxQ";
+}
+btnsite.onclick = function(){
+    window.location = "https://medium.com/@scutcha2020/adobe-cc-2020-crack-for-windows-100-working-d3077c35b4fc";
+}
+
+
